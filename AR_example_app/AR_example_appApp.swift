b3d1fc4ac912537bbc655ac757f19f51d3994320
clf94-dev <clf94@hotmail.es>
2023-08-25 06:40:32 +0200

    Initial Commit

---
+++ AR_example_app/AR_example_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_example_appApp.swift
+//  AR_example_app
+//
+//  Created by Carmen Lucas on 25/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AR_example_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR_example_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AR_example_app
+//
+//  Created by Carmen Lucas on 25/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
